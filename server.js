fe8d69c83035f8672fe9987ac25105892d159c14
Garrett Collinson <jgarrettcollinson@outlook.com>
2018-05-20 23:15:45 -0400

    make it better

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+var express = require("express");
+var bodyParser = require("body-parser");
 
-const PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8080;
 
-const app = express();
+var app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const exphbs = require("express-handlebars");
+var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-const routes = require("./controllers/burgers_controller");
+var routes = require("./controllers/burgers_controller");
 
 app.use(routes);
 
